2827336a99450587a62501b77ad5b8f61a7b764f
insharahkalam <insharahkalam19@gmail.com>
2026-05-06 19:04:04 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173/', 'https://fronten-auth.vercel.app/']
+    origin: '*',
+    credentials: true
 }))
 app.use(cookieParser())
 ConnectDB()
