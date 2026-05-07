b44a808594ed3a709d8dcbe35de5f50a047773e4
insharahkalam <insharahkalam19@gmail.com>
2026-05-07 20:41:44 +0500

    updated again

--- src/index.js
+++ src/index.js
@@ -13,9 +13,17 @@ const app = express()
 
 app.use(express.json())
 
+// app.use(cors({
+//     origin: ['http://localhost:5173', "https://fronten-auth.vercel.app/"],
+//     credentials: true
+// }))
+
+
 app.use(cors({
-    origin: ['http://localhost:5173', "https://fronten-auth.vercel.app/"],
-    credentials: true
+    origin: "https://fronten-auth.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(cookieParser())
