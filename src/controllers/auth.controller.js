c3d49f669d56867a0e7806bdb793a826d9d4da2e
insharahkalam <insharahkalam19@gmail.com>
2026-05-09 10:00:38 +0500

    updated

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -141,12 +141,9 @@ const loginUser = async (req, res) => {
 
     console.log("SECRET====>", process.env.JWT_SECRETS);
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        path: "/"
-    });
+    // const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("token", token);
 
     res.json({
         message: "User login successful",
@@ -165,13 +162,9 @@ const admin = async (req, res) => {
 
 const logOut = (req, res) => {
     try {
-        
-        res.clearCookie("token", {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-            path: "/"
-        });
+        // const isProduction = process.env.NODE_ENV === "production";
+
+        res.clearCookie("token");
 
         res.status(200).json({
             status: true,
