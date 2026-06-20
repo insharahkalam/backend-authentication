88f4d07422dc966b00370e6111cad0916d3c98f4
insharahkalam <insharahkalam19@gmail.com>
2026-06-20 16:41:45 +0500

    done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -204,7 +204,6 @@ const forgotPass = async (req, res) => {
     }
 
     const resetToken = crypto.randomBytes(32).toString("hex");
-
     user.resetToken = resetToken;
     user.resetTokenExpire = Date.now() + 15 * 60 * 1000;
     await user.save();
