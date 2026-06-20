a58a74a94f8c7458445c425311b14f0653261758
insharahkalam <insharahkalam19@gmail.com>
2026-06-21 01:56:47 +0500

    deployment issue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -149,7 +149,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        // sameSite: "none",
+        sameSite: "none",
         path: '/'
     });
 
